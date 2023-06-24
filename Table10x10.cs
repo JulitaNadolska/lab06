747eb6d86af880aca3d54994128f0d021eab312f
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2023-06-24 20:04:37 +0200

    refactor

--- Table10x10.cs
+++ Table10x10.cs
@@ -1,34 +1,38 @@
-﻿class Table10x10
+﻿class Program
 {
     static void Main()
     {
-        int[,] table = new int[10, 10];
+        Console.Write("Enter size of the table: ");
+        int tableSize = int.Parse(Console.ReadLine());
+
+        int[,] table = new int[tableSize, tableSize];
         int number = 1;
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < tableSize; i++)
         {
             if (i % 2 == 0)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < tableSize; j++)
                 {
                     table[i, j] = number++;
                 }
             }
             else
             {
-                for (int j = 9; j >= 0; j--)
+                for (int j = tableSize - 1; j >= 0; j--)
                 {
                     table[i, j] = number++;
                 }
             }
         }
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < tableSize; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < tableSize; j++)
             {
                 Console.Write(table[i, j] + " ");
             }
+
             Console.WriteLine();
         }
     }
