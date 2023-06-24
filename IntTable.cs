19cd6c093e6bf5e2e3cf1076d3439f5775c11c46
Julita <43172762+JulitaNadolska@users.noreply.github.com>
2023-06-24 20:08:45 +0200

    refactor

--- IntTable.cs
+++ IntTable.cs
@@ -2,32 +2,35 @@
 {
     static void Main()
     {
-        int[,] intTable = new int[10, 10];
+        Console.Write("Enter size of the table: ");
+        int tableSize = int.Parse(Console.ReadLine());
+
+        int[,] newTable = new int[tableSize, tableSize];
         int number = 1;
         int row = 0;
         int column = 0;
         int direction = 1;
 
-        while (number <= 100)
+        while (number <= tableSize * tableSize)
         {
-            intTable[row, column] = number++;
+            newTable[row, column] = number++;
 
-            if (direction == 1 && (column == 9 || intTable[row, column + 1] != 0))
+            if (direction == 1 && (column == tableSize - 1 || newTable[row, column + 1] != 0))
             {
                 direction = 2;
                 row++;
             }
-            else if (direction == 2 && (row == 9 || intTable[row + 1, column] != 0))
+            else if (direction == 2 && (row == tableSize - 1 || newTable[row + 1, column] != 0))
             {
                 direction = 3;
                 column--;
             }
-            else if (direction == 3 && (column == 0 || intTable[row, column - 1] != 0))
+            else if (direction == 3 && (column == 0 || newTable[row, column - 1] != 0))
             {
                 direction = 4;
                 row--;
             }
-            else if (direction == 4 && (row == 0 || intTable[row - 1, column] != 0))
+            else if (direction == 4 && (row == 0 || newTable[row - 1, column] != 0))
             {
                 direction = 1;
                 column++;
@@ -43,11 +46,11 @@
                 row--;
         }
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < tableSize; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < tableSize; j++)
             {
-                Console.Write(intTable[i, j] + " ");
+                Console.Write(newTable[i, j] + " ");
             }
             Console.WriteLine();
         }
